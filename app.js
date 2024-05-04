1578ebfe2f6865c4cd078e3d862c6cccb0bf1e02
Alex <al@appalanche.io>
2024-05-04 17:14:13 -0400

    JavaScript Operators Exercise: Complete

--- app.js
+++ app.js
@@ -4,16 +4,73 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+//console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(add);
+console.log(a);
+
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+//console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+
+console.log(num + str);
+// Output: 1111
+
+console.log(num + str2);
+// Output: 11eleven
+
+console.log(num + isPresent);
+// Output: 12
+
+console.log(firstName + num);
+// Output: Frodo11
+
+console.log(isPresent + str);
+// Output: true11
+
+console.log(firstName + lastName);
+// FrodoBaggins
+
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+//console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+console.log(al == str3);
+// Output: true
+console.log(val === str3);
+// Output: false
+console.log(!isPresent2);
+// Output: true
+console.log(“eleven” == str4 && val >= str3);
+// Output: false
+console.log(!isPresent2 || isPresent2);
+// Output: true
+console.log(0 == false);
+// Output: true
+console.log(0 === false);
+// Output: false
+console.log(0 != false);
+// Output: false
+console.log(0 !== false);
+// Output: true
